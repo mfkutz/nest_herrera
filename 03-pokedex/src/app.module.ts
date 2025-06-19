a97f9acd032803d7a40e37ae21e4fa8018b8ae4f
mfkutz <mfkutz@gmail.com>
2025-06-19 18:26:01 -0300

    joi and .env templates

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -2,17 +2,36 @@ import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+
+    //variables of environment
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot("mongodb://localhost:27017/nest-pokemon")
-    , PokemonModule, CommonModule, SeedModule],
+    MongooseModule.forRoot("mongodb://localhost:27017/nest-pokemon"),
+
+    PokemonModule,
+    CommonModule,
+    SeedModule],
+
+
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    // console.log(process.env) //here can see all variables of environment
+  }
+}
