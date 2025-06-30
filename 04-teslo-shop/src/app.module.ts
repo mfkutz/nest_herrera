8179032545591274157fc87976f2268e529ce7cf
mfkutz <mfkutz@gmail.com>
2025-06-30 13:27:38 -0300

    postgres configuration

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -16,7 +17,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true, //this in production is better to set to false
 
-    })
+    }),
+
+    ProductsModule
 
 
   ],
